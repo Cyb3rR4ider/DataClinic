686aac463727420f33e6bea0442c2b2c90f8a6cc
mtsou <m.tsoumenis@hotmail.com>
2024-05-02 15:48:00 +0300

    dynamic search user (update)

--- Functions/Users/SearchedResults.php
+++ Functions/Users/SearchedResults.php
@@ -31,11 +31,38 @@ if (isset($_POST['input'])) {  //value in POST come from ajax method as data opt
                     echo "<td>" . $us1->user_email = $row['user_email'] . "</td>";
                     echo "<td>" . $us1->user_tel = $row['user_tel'] . "</td>";
                     echo "<td>" . $us1->us_role_id = $row['us_role_id'] . "</td>";
-                    echo ("</tr>");
+                
                 }
 
                 ?>
+                   <td>
+                <form style ="float: left; padding: 2px;" method="post" action="index.php?action=4">
+                    <input style=" display:none ;color: red; width: 0px; height: 0px;" type="text" name="passiddel" value="<?php echo $row['user_id'] ?>" readonly>
+                    <button type="submit" title="Διαγραφή Χρήστη" class="btn btn-danger btn-primary">
+                        <i class="fa fa-trash"></i></button>
+                </form>
+                <form style ="float: left; padding: 2px;" method="post" action="index.php?action=5">
+                    <input style=" display:none ;color: red; width: 0px; height: 0px;" type="text" name="passidview" value="<?php echo $row['user_id'] ?>" readonly>
+                    <button type="submit" title="Προβολή Χρήστη" class="btn btn-info btn-primary">
+                        <i class="fa fa-eye"></i></button>
+                </form>
+                <form style ="float: left; padding: 2px;" method="post" action="index.php?action=6">
+                    <input style=" display:none ;color: red; width: 0px; height: 0px;" type="text" name="passidres" value="<?php echo $row['user_id'] ?>" readonly>
+                    <button type="submit" title="Reset Password" class="btn btn-success btn-primary">
+                        <i class="fa fa-undo"></i></button>
+                </form>
+                <form style ="float: left; padding: 2px;" method="post" action="index.php?action=11">
+                    <input style=" display:none ;color: red; width: 0px; height: 0px;" type="text" name="passidupdate" value="<?php echo $row['user_id'] ?>" readonly>
+                    <button type="submit" title="Edit User" class="btn btn-success btn-primary">
+                    <i class="fa fa-pencil-square" aria-hidden="true"></i>
+
+                </form>
+                </td>
+                
             </tbody>
+            <?php
+                echo ("</tr>");
+                ?>
         </table>
 <?php
     } else {
